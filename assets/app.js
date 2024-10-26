a55cba96709a37239f36bd9b728a0c52a75ee66f
Ihor <it@otpusk.com>
2024-10-26 19:02:35 +0300

    initial release with cursor

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,28 @@
 const cursor = document.getElementById('cursor');
 
 const positionElement = (e) => {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
+    const mouseY = e.pageY;
+    const mouseX = e.pageX;
 
     cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    cursor.style.opacity = '1'; // Показуємо курсор при русі миші
+}
+
+const hideCursor = () => {
+    cursor.style.opacity = '0'; // Ховаємо курсор при прокрутці
 }
 
 window.addEventListener('mousemove', positionElement);
+window.addEventListener('scroll', hideCursor);
 
-const hoverElement = document.getElementById('hoverElement');
+const hoverElements = document.querySelectorAll('.hoverElement');
 
-hoverElement.addEventListener('mouseenter', () => {
-    cursor.classList.add('hover');
-});
+hoverElements.forEach(hoverElement => {
+    hoverElement.addEventListener('mouseenter', () => {
+        cursor.classList.add('hover');
+    });
 
-hoverElement.addEventListener('mouseleave', () => {
-    cursor.classList.remove('hover');
+    hoverElement.addEventListener('mouseleave', () => {
+        cursor.classList.remove('hover');
+    });
 });
